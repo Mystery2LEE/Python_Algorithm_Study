bd3f3dbd314d6dbc203109e2cb12e621198c4e37
Parkjungsu1 <a62627629@gmail.com>
2026-08-05 20:21:01 +0900

    add week02 folder

---
+++ week02/박정수/SWEA_26931_컨베이어벨트상자정리.py
@@ -0,0 +1,17 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    s = input().strip()
+    stack = []
+
+    for i in range(len(s)):
+        c = s[i]
+        if not stack :
+            stack.append(c)
+            continue
+
+        if c == stack[-1]:
+            stack.pop()
+        else :
+            stack.append(c)
+    
+    print(f"#{test_case} {len(stack)}")
